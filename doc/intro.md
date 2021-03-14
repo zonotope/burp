b32e6ed61709603ecf14a4bb77a7de2c1df27813
ben lamothe <bl@732.one>
2021-03-14 12:02:39 -0400

    initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to burp
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/one/732/burp_test.clj
@@ -0,0 +1,7 @@
+(ns one.732.burp-test
+  (:require [clojure.test :refer :all]
+            [one.732.burp :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
